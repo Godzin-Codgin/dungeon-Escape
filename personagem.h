a35c279651c94d4a37b0af12b234a96239e586cd
DAVI MORETTO <davi_moretto@estudante.sesisenai.org.br>
2026-06-02 16:37:28 -0300

    manutenção de personagem

--- personagem.h
+++ personagem.h
@@ -4,20 +4,18 @@
 
 using namespace std;
 
-class personagem {
-	
-	public:
+class Personagem {
+public:
 	string nome;
 	int vida;
 	int ataque;
     
     
-    Personagem (string n,int v,int a): nome(n), vida(v), ataque(a) {}
+    Personagem (string n, int v, int a): nome(n), vida(v), ataque(a) {};
     
     void apresentar(){
     	cout<<"personagem: "<< nome <<endl;
     	cout<< "vida: "<< vida <<endl;
     	cout<<"ataque: "<< ataque<<endl;
 	}
- return 0;
-}
+};
