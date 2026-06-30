075a6bc4b3b4d6f329caa9d5d794d2f24bc8a840
Aluno SENAI <Aluno@SN-405089.edu.fiesc.com.br>
2026-06-30 14:49:20 -0300

    feito defesa

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
-#include "combate.h" 
-#include "personagem.h"
+#include "combate.h"
 
 using namespace std;
 
 int main() {
-   
-   Personagem heroi("herói", 100, 10);
-    heroi.apresentar();
     
-    Inimigo goblin("Goblin", 30, 5, "Goblin");
-    goblin.apresentar();
-    
+    string nomePersonagem = "Godzin prozin";
+    int vidaPersonagem = 100;
+    int ataquePersonagem = 25;
 
-    
+   
     exibir(nomePersonagem, vidaPersonagem, ataquePersonagem);
 
+    
+    int defesaInimigo = 15; 
+    
+   
+    int danoCausado = dano(ataquePersonagem, defesaInimigo);
+
+    
+    cout << "\n--- Resultado do Ataque ---" << endl;
+    cout << "O personagem atacou com " << ataquePersonagem << " de ataque." << endl;
+    cout << "O inimigo se defendeu com " << defesaInimigo << " de defesa." << endl;
+    cout << "Dano real causado ao inimigo: " << danoCausado << endl;
+    cout << "----------------------------" << endl;
 
     system("pause"); 
     return 0;
